fd6f3a38c721d7fc2c802a06117b0798332fe080
cjal95 <60397509+cjal95@users.noreply.github.com>
2021-11-29 08:05:21 +0100

    Had to look up solution, couldn't get my version working

--- day2/day2.go
+++ day2/day2.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"regexp"
 )
 
+type password struct {
+	min, max         int
+	letter, password string
+}
+
 func main() {
-	fmt.Println("Hello")
+
+	file, err := os.Open("input.txt")
+	if err != nil{
+		log.Fatal(err) //If error in opening file
+	}
+	defer file.Close()
+	passwords := []*password{}
+
+	inputRegex := regexp.MustCompile(`^(\d+)-(\d+) (\w): (\w+)$`)
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		min, _ := strconv.Atoi(inputRegex.ReplaceAllString(text, "$1"))
+		max, _ := strconv.Atoi(inputRegex.ReplaceAllString(text, "$2"))
+		passwords = append(passwords, &password{
+			min:      min,
+			max:      max,
+			letter:   inputRegex.ReplaceAllString(text, "$3"),
+			password: inputRegex.ReplaceAllString(text, "$4"),
+		})
+	}
+	correct := 0
+	for _, pw := range passwords {
+		if count := strings.Count(pw.password, pw.letter); count >= pw.min && count <= pw.max {
+		correct++
+		}
+	}
+	fmt.Println(correct)
 }
